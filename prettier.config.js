69c87f73a68a544fe070d5236ea8e80937abbd29
Mike Lee <mike.lee@partnerstack.com>
2022-01-30 13:35:09 -0500

    Configure ESLint and Prettier

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 100,
+  proseWrap: 'always',
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  semi: true,
+  htmlWhitespaceSensitivity: 'ignore',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  arrowParens: 'avoid',
+};
