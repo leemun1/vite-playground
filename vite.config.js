be7e9443aca81441e955f304a004cf585b28e819
Mike Lee <mike.lee@partnerstack.com>
2022-01-31 19:21:42 -0500

    Tell vite how to resolve @ for 'src' dir

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
 
+const path = require('path');
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
-  resolve: { alias: { vue: 'vue/dist/vue.esm-bundler.js' } },
+  resolve: {
+    alias: [
+      { find: 'vue', replacement: 'vue/dist/vue.esm-bundler.js' },
+      { find: '@', replacement: path.resolve(__dirname, '/src') },
+    ],
+  },
 });
